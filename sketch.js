57136b21455ac1c23936ad0ec7f8f0b5bb316130
Epicgeckogaming <47697210+Epicgeckogaming@users.noreply.github.com>
2020-12-20 19:34:36 -0500

    2nd Atempt
    Fun

--- sketch.js
+++ sketch.js
@@ -3,18 +3,27 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1, log1;
+var box1, pig1, log1, bird1;
+var box2, box3, box4, box5, log1, log2, log4,ground, log3,
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    box1 = new Box(200,300,50,50);
-    box2 = new Box(240,100,50,100);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
     ground = new Ground(600,height,1200,20)
-    pig1 = new Pig(200,200);
-    log1 = new log(100,300,100,PI/2);
+    pig1 = new Pig(810,350);
+    log1 = new Log(810,260,300,PI/2);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig2 = new Pig(810,320);
+    log2 = new Log(810, 180,300,PI/2);
+    log3 = new Log(760, 120,150,PI/2);
+    log4 = new Log(870, 120,150,PI/2);
+    box5 = new Box(810,160,70,70);
+    bird1 = new Bird(100,100);
 }
 
 function draw(){
@@ -28,4 +37,10 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();
+    log3.display();
+    log4.display();
 }
